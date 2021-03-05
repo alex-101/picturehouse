5f6588a8adefebee8181d48196746ba17983de0e
alex-101 <68401423+alex-101@users.noreply.github.com>
2021-03-05 16:26:57 +0000

    Add files via upload

---
+++ database.js
@@ -0,0 +1,26 @@
+var dataBase = [
+	{
+		"name": "Welcome to Picture House.",
+		"date": "2021",
+		"description": "If you arrived here, it is likely because you tried to watch an old or deleted video.",
+		"tags": "",
+		"videoId": "ayf1sYiNLhQ"
+	},
+	
+	{
+		"name": "Oh, Mr Porter!",
+		"date": "1937",
+		"description": "With the help of a relative, a hopeless railway employee is made stationmaster of Buggleskelly. Determined to make his mark, he devises a number of schemes to put Buggleskelly on the railway map, but instead falls foul of a gang of gun runners.",
+		"tags": "train, Buggleskelly, railway, gun runners",
+		"videoId": "JpvXq6yV1oA"
+	},
+
+	{
+		"name": "3",
+		"date": "4",
+		"description": "no",
+		"tags": "",
+		"videoId": "ayf1sYiNLhQ"
+	}
+]
+
